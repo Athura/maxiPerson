06a99fe592fd05fc47a546adbfa23237e70fbe81
Athura <joshualjohnson33@gmail.com>
2019-01-19 22:06:22 -0500

    PureComponents and fragment implementation

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 import ErrorBoundary from '../../ErrorBoundary/ErrorBoundary';
 
-class Persons extends Component {
+class Persons extends PureComponent {
     constructor(props) {
         super(props);
         console.log('[Persons.js] inside constructor', props);
@@ -12,23 +12,33 @@ class Persons extends Component {
         console.log('Inside component will mount');
     }
 
+    // This method runs after the component output has been rendered to the DOM
     componentDidMount() {
         console.log('Inside component did mount');
     }
 
     componentWillReceiveProps(nextProps) {
         console.log('[Update persons.js]', nextProps);
-        // return false or true normally
-        // We return false when our application shouldn't update
-        // We return true when our app should update
-
     }
 
-    shouldComponentUpdate = (nextProps, nextState) => {
-        console.log('Update persons.js inside should component update', nextProps, nextState);
+    // PureComponents have the below kind of check already built in so we don't need this
+    // shouldComponentUpdate = (nextProps, nextState) => {
+    //     console.log('Update persons.js inside should component update', nextProps, nextState);
+    //     return nextProps.persons !== this.props.persons ||
+    //     nextProps.changed !== this.props.changed ||
+    //     nextProps.clicked !== this.props.clicked;
+    //     // return true;
+    // }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('Persons component will now update');
     }
 
+    componentDidUpdate() {
+        console.log('Persons: Component did update');
+    }
 
+    //
     componentWillUnmount = () => {
         console.log('I\'m unmounting persons!');
     }
---
+++ src/components/hoc/_Aux.js
@@ -0,0 +1,6 @@
+// This is the same as using a React Fragment.
+// < >
+// < >
+const aux = (props) => props.children;
+
+export default aux;
